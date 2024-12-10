87f19b61fec51b44b0bc253e3fde786b5e7cee2f
Your Name <aggressivebanda74@gmail.com>
2024-12-10 11:03:24 +0545

    commeting

--- src/app/port.js
+++ src/app/port.js
@@ -1,6 +1,8 @@
-export const base = 'https://fp-z3nb.onrender.com/'
+export const base = 'https://backs-0h7q.onrender.com'
 export const baseUrl = `${base}/api`
 // https://fp-z3nb.onrender.com - render
 
 // http://192.168.18.108:1999 --localhost
 // https://fp-z3nb.onrender.com/
+
+// https://backs-0h7q.onrender.com
